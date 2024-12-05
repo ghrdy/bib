1fb7c5d2864cf33c75f5edbcfc9bb2b0a00810ae
Tim Hrdy <75643812+ghrdy@users.noreply.github.com>
2024-12-05 02:24:15 +0000

    s

--- back/src/models/Image.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const imageSchema = new mongoose.Schema({
-  url: String,
-});
-
-export default mongoose.model("Image", imageSchema);
--- front/README.md
+++
@@ -1,3 +0,0 @@
-# arasoft-bolt-front
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ghrdy/arasoft-bolt-front)
