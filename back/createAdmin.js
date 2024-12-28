a83e040d54d7890da2d7872cfe345f17a9486142
Tim Hrdy <tim.hrdy.1@gmail.com>
2024-12-28 16:42:03 +0100

    testing

---
+++ back/createAdmin.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import User from "./models/User.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://mongo:27017/dev";
+
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", async () => {
+  console.log("Connected to MongoDB");
+
+  const adminEmail = "tim.hrdy.1@gmail.com";
+  const adminPassword = "password";
+
+  const existingAdmin = await User.findOne({ email: adminEmail });
+
+  if (!existingAdmin) {
+    const hashedPassword = await bcrypt.hash(adminPassword, 10);
+    const adminUser = new User({
+      nom: "Admin",
+      prenom: "User",
+      email: adminEmail,
+      password: hashedPassword,
+      role: "admin",
+    });
+
+    await adminUser.save();
+    console.log("Admin user created");
+  } else {
+    console.log("Admin user already exists");
+  }
+
+  mongoose.connection.close();
+});
